143b9f339f27ee79c83253ada3c4494e7e9c4ec7
Kozyar Valeriy <monqpepers@gmail.com>
2019-12-17 22:06:27 +0300

    Добавьте файлы проекта.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(93, 133);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(575, 181);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 70);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(158, 200);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 10);
+            this.panel3.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 128);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(705, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 128);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 50;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(804, 574);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeBegin);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+
+    {
+        int gap = 60;
+        int dx = 10, dy = 10;
+        Recquet right_rec;
+        Recquet left_rec;
+
+        public Form1()
+        { 
+        InitializeComponent();
+            this.MouseWheel += new MouseEventHandler(this_MouseWheel);
+            this.MouseMove += new MouseEventHandler(this_MouseMove); //или по таймеру      
+            
+            void this_MouseMove(object sender, MouseEventArgs e)
+            {
+                this_MouseWheel(this, e);
+            }
+
+            void this_MouseWheel(object sender, MouseEventArgs e)
+            {
+                if (e.Delta > 0)
+                    left_rec.move_up();
+                else if (e.Delta < 0)
+                    left_rec.move_down();
+   
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width = this.ClientRectangle.Width;
+            int height = this.ClientRectangle.Height;
+            left_rec = new Recquet(width, height, gap, 10, panel1, "left");
+            right_rec = new Recquet(width, height, gap, 10, panel2, "right");
+            label1.Top = 0;
+            label1.Left = 0;
+            label2.Top = 0;
+            label2.Left = width - label2.Width;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyCode.ToString());
+            if ((e.KeyCode == Keys.W) || (e.KeyCode == Keys.Up)) {
+                left_rec.move_up();
+            } else if ((e.KeyCode == Keys.S) || (e.KeyCode == Keys.Down)) {
+                left_rec.move_down();
+            }
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            this.Form1_Load(this,null);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (panel3.Left >= this.ClientRectangle.Width / 2)
+            {
+                if (panel3.Top + panel3.Height / 2 > right_rec.Body.Top + right_rec.Body.Height / 2)
+                {
+                    right_rec.move_down();
+                } else
+                {
+                    right_rec.move_up();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((panel3.Top + dy <= 0) && (dy < 0))
+            {
+                panel3.Top = 0;
+                dy = -dy;
+            }
+            else if ((panel3.Top + panel3.Size.Height + dy >= this.ClientRectangle.Height) && (dy > 0)) 
+            {
+                panel3.Top = this.ClientRectangle.Height - panel3.Size.Height;
+                dy = -dy;
+            }
+            else
+            {
+                panel3.Top += dy;
+            }
+            
+            if ((panel3.Left + dx <= left_rec.Body.Left + left_rec.Body.Size.Width) && (dx < 0))
+            {
+                panel3.Left = left_rec.Body.Left + left_rec.Body.Size.Width;
+
+                if ((panel3.Top <= left_rec.Body.Top + left_rec.Body.Height) &&
+                    (panel3.Top + panel3.Height >= left_rec.Body.Top))
+                {
+                    dx = -dx;
+                } else
+                {
+                    if (label1.Text == "9")
+                    {
+                        timer1.Enabled = false;
+                        return;
+                    }
+                    label1.Text = (int.Parse(label1.Text) + 1).ToString();
+                    dx = -dx;
+                    panel3.Top = this.ClientRectangle.Height / 2;
+                    panel3.Left = this.ClientRectangle.Width / 2;
+                    timer1.Enabled = false;
+                    timer2.Enabled = true;
+                }
+
+                
+            }
+            else if ((panel3.Left + panel3.Size.Width + dx >= right_rec.Body.Left) && (dx > 0))
+            {
+                panel3.Left = right_rec.Body.Left - panel3.Size.Width;
+                if ((panel3.Top <= right_rec.Body.Top + right_rec.Body.Height) &&
+                       (panel3.Top + panel3.Height >= right_rec.Body.Top))
+                {
+                    dx = -dx;
+                }
+                else
+                {
+                    if (label2.Text == "9")
+                    {
+                        timer1.Enabled = false;
+                        return;
+                    }
+                    label2.Text = (int.Parse(label2.Text) + 1).ToString();
+                    dx = -dx;
+                    panel3.Top = this.ClientRectangle.Height / 2;
+                    panel3.Left = this.ClientRectangle.Width / 2;
+
+                    timer1.Enabled = false;
+                    timer2.Enabled = true;
+                }
+
+            }
+            else
+            {
+                panel3.Left += dx;
+            }
+
+
+
+           
+        }
+    }
+
+    //interface Recquet
+    //{
+    //   void move_up();
+
+    //}
+
+    public class Recquet
+    {
+        //public int Offset { get; set; }
+        public int Dy { get; set; }
+        public int Client_width { get; set; }
+        public int Client_height { get; set; }
+        public Panel Body { get; set; }
+
+        public Recquet(int width, int height, int offset, int dy, Panel body, string position)
+        {
+            //this.Offset = offset;
+            this.Dy = dy;
+            this.Body = body;
+            this.Client_width = width;
+            this.Client_height = height;
+
+            if (position == "left")
+            {
+                this.Body.Left = offset;
+            }
+            else
+            {
+                this.Body.Left = this.Client_width - offset - this.Body.Size.Width;
+                //Form1.ActiveForm.ClientSize.Width
+            }
+        }
+
+        public void move_up()
+        {
+            if (this.Body.Top >= this.Dy)
+            {
+                this.Body.Top -= this.Dy;
+            }
+            else
+            {
+                this.Body.Top = 0;
+            }
+        }
+
+        public void move_down()
+        {
+            if (this.Body.Top + this.Body.Size.Height + this.Dy <= this.Client_height)
+            {
+                this.Body.Top += this.Dy;
+            }
+            else
+            {
+                this.Body.Top = this.Client_height - this.Body.Size.Height;
+            }
+        }
+    }
+}
