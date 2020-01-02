fd289f6803928d052e66c141a283855b228a5885
Valeriy Kozyar <51660599+KozyarValeriy@users.noreply.github.com>
2020-01-02 12:29:52 +0300

    Add files via upload

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -37,6 +37,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -44,7 +54,7 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(93, 133);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(10, 70);
+            this.panel1.Size = new System.Drawing.Size(15, 70);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -52,7 +62,7 @@
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Location = new System.Drawing.Point(575, 181);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(10, 70);
+            this.panel2.Size = new System.Drawing.Size(15, 70);
             this.panel2.TabIndex = 1;
             // 
             // panel3
@@ -76,7 +86,7 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 128);
+            this.label1.Size = new System.Drawing.Size(50, 59);
             this.label1.TabIndex = 3;
             this.label1.Text = "0";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -88,7 +98,7 @@
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(705, 181);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 128);
+            this.label2.Size = new System.Drawing.Size(50, 59);
             this.label2.TabIndex = 4;
             this.label2.Text = "0";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -104,23 +114,147 @@
             this.timer3.Interval = 50;
             this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
             // 
+            // button1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.button1, 2);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 56);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Resume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.trackBar1, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(260, 27);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(264, 389);
+            this.tableLayoutPanel2.TabIndex = 6;
+            this.tableLayoutPanel2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tableLayoutPanel2.SetColumnSpan(this.button2, 2);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(3, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 56);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Black;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.Location = new System.Drawing.Point(108, 103);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(153, 79);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(108, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 80);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0:0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 80);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Score";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 85);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Speed";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.label3, 2);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Impact", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 100);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Pause";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(804, 574);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Pong";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
-            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeBegin);
+            this.SizeChanged += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,6 +270,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -8,29 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pong
-{
-    public partial class Form1 : Form
-
-    {
+namespace Pong {
+    public partial class Form1 : Form {
         int gap = 60;
         int dx = 10, dy = 10;
         Recquet right_rec;
         Recquet left_rec;
 
-        public Form1()
-        { 
+        public Form1() { 
         InitializeComponent();
             this.MouseWheel += new MouseEventHandler(this_MouseWheel);
             this.MouseMove += new MouseEventHandler(this_MouseMove); //или по таймеру      
             
-            void this_MouseMove(object sender, MouseEventArgs e)
-            {
+            void this_MouseMove(object sender, MouseEventArgs e) {
                 this_MouseWheel(this, e);
             }
 
-            void this_MouseWheel(object sender, MouseEventArgs e)
-            {
+            void this_MouseWheel(object sender, MouseEventArgs e) {
                 if (e.Delta > 0)
                     left_rec.move_up();
                 else if (e.Delta < 0)
@@ -39,107 +33,138 @@ namespace Pong
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
             int width = this.ClientRectangle.Width;
             int height = this.ClientRectangle.Height;
+            if (panel1.Top + panel1.Height > height) {
+                panel1.Top = height - panel1.Height;
+            }
+            if (panel2.Top + panel2.Height > height) {
+                panel2.Top = height - panel2.Height;
+            }
             left_rec = new Recquet(width, height, gap, 10, panel1, "left");
             right_rec = new Recquet(width, height, gap, 10, panel2, "right");
             label1.Top = 0;
             label1.Left = 0;
             label2.Top = 0;
             label2.Left = width - label2.Width;
-        }
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
+            Pen p = new Pen(Color.Blue, 5);// цвет линии и ширина
+            Point p1 = new Point(5, 10);// первая точка
+            Point p2 = new Point(40, 100);// вторая точка
+
+ 
+
+    }
+
+    private void Form1_KeyDown(object sender, KeyEventArgs e) {
             //MessageBox.Show(e.KeyCode.ToString());
             if ((e.KeyCode == Keys.W) || (e.KeyCode == Keys.Up)) {
                 left_rec.move_up();
             } else if ((e.KeyCode == Keys.S) || (e.KeyCode == Keys.Down)) {
                 left_rec.move_down();
+            } else if ((e.KeyCode == Keys.P) || (e.KeyCode == Keys.Space)) {
+               // if (!tableLayoutPanel2.Visible) {
+                    timer1.Enabled = !timer1.Enabled;
+                    timer3.Enabled = !timer3.Enabled;
+                    label6.Text = label1.Text + " : " + label2.Text;
+                    tableLayoutPanel2.Left = this.Width / 2 - tableLayoutPanel2.Width / 2;
+                    tableLayoutPanel2.Top = this.Height / 2 - tableLayoutPanel2.Height / 2;
+                    tableLayoutPanel2.Visible = !tableLayoutPanel2.Visible;
+                    
+               // }
+
             }
         }
 
-        private void Form1_ResizeBegin(object sender, EventArgs e)
-        {
-            this.Form1_Load(this,null);
-        }
 
-        private void timer2_Tick(object sender, EventArgs e)
-        {
+        private void timer2_Tick(object sender, EventArgs e) {
             timer1.Enabled = true;
+            dy = 10;
+            dx = 10;
+            timer2.Enabled = false;
         }
 
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            if (panel3.Left >= this.ClientRectangle.Width / 2)
-            {
-                if (panel3.Top + panel3.Height / 2 > right_rec.Body.Top + right_rec.Body.Height / 2)
-                {
+        private void timer3_Tick(object sender, EventArgs e) {
+            if ((panel3.Left >= this.ClientRectangle.Width / 2) && (dx > 0)) {
+                if (panel3.Top + panel3.Height / 2 > right_rec.Body.Top + right_rec.Body.Height / 2) {
                     right_rec.move_down();
-                } else
-                {
+                } else {
                     right_rec.move_up();
                 }
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if ((panel3.Top + dy <= 0) && (dy < 0))
-            {
+        private void button1_Click(object sender, EventArgs e) {
+
+            tableLayoutPanel2.Visible = !tableLayoutPanel2.Visible;
+            timer1.Enabled = !timer1.Enabled;
+            timer3.Enabled = !timer3.Enabled;
+            timer1.Interval = 80 - trackBar1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            dx = 10;
+            dy = 10;
+            panel3.Top = this.ClientRectangle.Height / 2;
+            panel3.Left = this.ClientRectangle.Width / 2;
+            label2.Text = "0";
+            label1.Text = "0";
+
+            this.button1_Click(this, null);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            if ((panel3.Top + dy <= 0) && (dy < 0)) {
                 panel3.Top = 0;
                 dy = -dy;
-            }
-            else if ((panel3.Top + panel3.Size.Height + dy >= this.ClientRectangle.Height) && (dy > 0)) 
-            {
+            } else if ((panel3.Top + panel3.Size.Height + dy >= this.ClientRectangle.Height) && (dy > 0)) {
                 panel3.Top = this.ClientRectangle.Height - panel3.Size.Height;
                 dy = -dy;
-            }
-            else
-            {
+            } else {
                 panel3.Top += dy;
             }
             
-            if ((panel3.Left + dx <= left_rec.Body.Left + left_rec.Body.Size.Width) && (dx < 0))
-            {
+            if ((panel3.Left + dx <= left_rec.Body.Left + left_rec.Body.Size.Width) && (dx < 0)) {
                 panel3.Left = left_rec.Body.Left + left_rec.Body.Size.Width;
 
                 if ((panel3.Top <= left_rec.Body.Top + left_rec.Body.Height) &&
-                    (panel3.Top + panel3.Height >= left_rec.Body.Top))
-                {
+                    (panel3.Top + panel3.Height >= left_rec.Body.Top)) {
                     dx = -dx;
-                } else
-                {
-                    if (label1.Text == "9")
-                    {
+                } else {
+                    if (label1.Text == "9") {
                         timer1.Enabled = false;
+                        timer3.Enabled = false;
+                        tableLayoutPanel2.Visible = true;
+                        label6.Text = label1.Text + " : " + label2.Text;
+                        button1.Enabled = false;
                         return;
                     }
                     label1.Text = (int.Parse(label1.Text) + 1).ToString();
                     dx = -dx;
+                   
                     panel3.Top = this.ClientRectangle.Height / 2;
                     panel3.Left = this.ClientRectangle.Width / 2;
                     timer1.Enabled = false;
                     timer2.Enabled = true;
                 }
 
-                
-            }
-            else if ((panel3.Left + panel3.Size.Width + dx >= right_rec.Body.Left) && (dx > 0))
-            {
+                dy = Math.Sign(dy) * CounterIncremet.Increment(panel3.Size.Height / 2 + panel3.Top,
+                                                               left_rec.Body.Height / 2 + left_rec.Body.Top,
+                                                               left_rec.Body.Height, dy);
+
+            } else if ((panel3.Left + panel3.Size.Width + dx >= right_rec.Body.Left) && (dx > 0)) {
                 panel3.Left = right_rec.Body.Left - panel3.Size.Width;
                 if ((panel3.Top <= right_rec.Body.Top + right_rec.Body.Height) &&
-                       (panel3.Top + panel3.Height >= right_rec.Body.Top))
-                {
+                       (panel3.Top + panel3.Height >= right_rec.Body.Top)) {
                     dx = -dx;
-                }
-                else
-                {
-                    if (label2.Text == "9")
-                    {
+                } else {
+                    if (label2.Text == "9") {
                         timer1.Enabled = false;
+                        timer3.Enabled = false;
+                        tableLayoutPanel2.Visible = true;
+                        label6.Text = label1.Text + " : " + label2.Text;
+                        button1.Enabled = false;
                         return;
                     }
                     label2.Text = (int.Parse(label2.Text) + 1).ToString();
@@ -151,15 +176,35 @@ namespace Pong
                     timer2.Enabled = true;
                 }
 
-            }
-            else
-            {
+                dy = Math.Sign(dy) * CounterIncremet.Increment(panel3.Size.Height / 2 + panel3.Top,
+                                                               right_rec.Body.Height / 2 + right_rec.Body.Top,
+                                                               right_rec.Body.Height, dy);
+
+            } else {
+                
                 panel3.Left += dx;
             }
 
-
-
            
+        }
+
+       
+    }
+
+    static class CounterIncremet {
+        public static int Increment(int center_ball, int center_recquet, int length_recquet, int dy) {
+            int delta;
+            
+            if (dy < 0) {
+                delta = 10 +  5 *(center_ball - center_recquet) / (length_recquet / 2);
+
+            } else {
+                delta = 10 +  5 * (center_recquet - center_ball) / (length_recquet / 2);
+
+            }
+
+            return delta;
+
         }
     }
 
@@ -169,53 +214,40 @@ namespace Pong
 
     //}
 
-    public class Recquet
-    {
+    public class Recquet {
         //public int Offset { get; set; }
         public int Dy { get; set; }
         public int Client_width { get; set; }
         public int Client_height { get; set; }
         public Panel Body { get; set; }
 
-        public Recquet(int width, int height, int offset, int dy, Panel body, string position)
-        {
+        public Recquet(int width, int height, int offset, int dy, Panel body, string position) {
             //this.Offset = offset;
             this.Dy = dy;
             this.Body = body;
             this.Client_width = width;
             this.Client_height = height;
 
-            if (position == "left")
-            {
+            if (position == "left") {
                 this.Body.Left = offset;
-            }
-            else
-            {
+            } else {
                 this.Body.Left = this.Client_width - offset - this.Body.Size.Width;
                 //Form1.ActiveForm.ClientSize.Width
             }
         }
 
-        public void move_up()
-        {
-            if (this.Body.Top >= this.Dy)
-            {
+        public void move_up() {
+            if (this.Body.Top >= this.Dy) {
                 this.Body.Top -= this.Dy;
-            }
-            else
-            {
+            } else {
                 this.Body.Top = 0;
             }
         }
 
-        public void move_down()
-        {
-            if (this.Body.Top + this.Body.Size.Height + this.Dy <= this.Client_height)
-            {
+        public void move_down() {
+            if (this.Body.Top + this.Body.Size.Height + this.Dy <= this.Client_height) {
                 this.Body.Top += this.Dy;
-            }
-            else
-            {
+            } else {
                 this.Body.Top = this.Client_height - this.Body.Size.Height;
             }
         }
